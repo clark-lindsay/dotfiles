d19cdf5e676f1a2557472605eb476fd72fc3e29b
clark-lindsay <clark.lindsay@integral.io>
2022-02-05 16:07:17 -0500

    Add `fish` shell config directory

---
+++ fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export VIMCONFIG=~/.config/nvim
+export VIMDATA=~/.local/share/nvim
+set -U EDITOR nvim
+
+nvm use v16.11 1> /dev/null
+
+function re_source
+    source ~/.config/fish/config.fish
+end
+
+
+# fzf start
+fzf_configure_bindings --git_log=\cg --directory=\ct --history=\ch --variables=\cv --git_status=\ca
+#fzf end
+
+# nintendo joy-con blue and red 
+set -g hydro_color_prompt "00C3E3"
+set -g hydro_color_error  "FF4554"
+# soft yellow
+set -g hydro_color_git            "E2DF70"
+set -g hydro_fetch                true
+set -g hydro_multiline            true
+set -g fish_prompt_pwd_dir_length 2
---
+++ fish/functions/gapa.fish
@@ -0,0 +1,1 @@
+alias "gapa" "git add --patch"
