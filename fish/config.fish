678a2c9d7846062b49a5774a5799c17261496974
clark-lindsay <clarklindsay96@gmail.com>
2024-03-04 14:25:04 -0500

    update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,19 +4,24 @@ end
 
 set -x VIMCONFIG "~/.config/nvim"
 set -x VIMDATA   "~/.local/share/nvim"
-set -x EDITOR    nvim
+set -x EDITOR    lvim
 
-nvm use v16.11 1> /dev/null
+### asdf start 
+# `asdf` instructions for fish compatability, per: https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
+source /usr/local/opt/asdf/libexec/asdf.fish
+### asdf end 
 
-function re_source
-    source ~/.config/fish/config.fish
-end
+### homebrew start
+set -x HOMEBREW_PREFIX "/usr/local"
+### homebrew end
 
+fish_add_path /usr/local/sbin
 
-# fzf start
+### fzf start
 fzf_configure_bindings --git_log=\cg --directory=\ct --history=\ch --variables=\cv --git_status=\ca --processes=\cp
-#fzf end
+### fzf end
 
+### promt start
 # nintendo joy-con blue and red 
 set -g hydro_color_prompt         "00C3E3"
 set -g hydro_color_error          "FF4554"
@@ -27,4 +32,10 @@ set -g hydro_color_duration       "5E819D"
 set -g hydro_fetch                true
 set -g hydro_multiline            true
 set -g fish_prompt_pwd_dir_length 2
-fish_add_path /usr/local/sbin
+### prompt end
+
+### Nix start
+if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+end
+### Nix end
---
+++ fish/functions/gsw.fish
@@ -0,0 +1,1 @@
+alias "gsw" "git switch"
